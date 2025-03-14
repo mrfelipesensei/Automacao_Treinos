927669cb40441d8b12b34644bd6830ed78b3c4e2
Felipe Greg <mrfelipesensei@gmail.com>
2025-03-14 17:37:26 -0300

    exercicios = [...]

---
+++ index.py
@@ -0,0 +1,16 @@
+import random
+
+#Grupos musculares e exercícios
+exercicios = {
+    'Peito': ['Crucifixo','Supino Sentado','Flexão de Braço','Flexão Declinada'],
+    'Costas': ['Puxada Alta','Remada Baixa','Puxada Alta Triângulo','Remada Curvada'],
+    'Perna': ['Extensora','Agachamento','Stiff','Panturilha'],
+    'Ombro': ['Desenvolvimento','Elevação Lateral','Elevação Frontal','Arnold Press'],
+    'Bíceps': ['Rosca Direta','Rosca Alternada','Rosca Martelo'],
+    'Tríceps': ['Tríceps Polia','Mergulho','Tríceps Testa'],
+    'Antebraço': ['Rosca de Punho','Rosca Direta Invertida'],
+    'Trapézio': ['Remada Alta','Encolhimento Ombro','Face Pull'],
+    'Abdominal': ['Reto','Bicicleta','Rotação Russa','Prancha','Alpinista'],
+    'Cárdio' : ['Corrida','Pular Corda','Funcional','Elíptico']
+
+}
